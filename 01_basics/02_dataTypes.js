ef522e2c11bf4694ea56fce24273808070bce035
Riya Raj <mail2riya02@gmail.com>
2026-04-05 02:59:15 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all js code as newer version
+
+// alert (3+3) //we are using node js not browser and it can be written in another for for 
+// codde redability should be high so use indentation
+let name = "riya" //string
+let age = 18        //number
+let isLoggedIn = false  //boolean
+let state;
+
+// number => 2 to power 53 (range)
+// bigint ...vry big num like trading
+// string => ""
+// boolean => true/false
+// null => standalone value (presentation of empty value)
+// undefined => variable declared but value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
